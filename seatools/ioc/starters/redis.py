0bb2f9de761ac73d7a74200d2aa28a6618179b2b
dragons96 <jinqunlong@black-unique.com>
2024-12-10 17:37:03 +0800

    update: 使用`seatools.redis`优化

--- seatools/ioc/starters/redis.py
+++ seatools/ioc/starters/redis.py
@@ -1,17 +1,8 @@
-from typing import Optional
-
 from seatools.ioc.config import cfg
-from seatools.ioc.database import DatabaseConfig
+from seatools.redis.dbconfig import RedisConfig
+from seatools.redis.utils import new_redis
 from seatools.ioc.injects import Bean
 from loguru import logger
-
-import redis
-
-
-class RedisConfig(DatabaseConfig):
-    """通用 DB 配置"""
-    config: Optional[dict] = None
-    driver: Optional[str] = 'redis'
 
 
 @Bean
@@ -29,16 +20,9 @@ def init_redis():
         exit(1)
     try:
         config = RedisConfig(**redis_config)
-        client = __new_redis_client(config, config.config)
+        client = new_redis(config, config.config)
         # 注册bean
         Bean(name=config.name or 'redis', primary=config.primary)(client)
     except Exception as e:
         logger.error(f'配置[seatools.redis]存在不支持的参数, 请检查修改配置后重试')
         exit(1)
-
-
-
-def __new_redis_client(config: RedisConfig, extra_config: Optional[dict] = None):
-    url = config.render_to_string(hide_password=False)
-    client = redis.Redis.from_url(url, **(extra_config or {}))
-    return client
